6d7f0f2acc8c5a487bdba39711f3d4d8697affaf
BakinII <bakin_ii@mail.ru>
2022-11-27 15:11:20 +0700

    start

---
+++ Table.cpp
@@ -0,0 +1,5 @@
+//
+// Created by иван on 27.11.2022.
+//
+
+#include "Table.h"
---
+++ Table.h
@@ -0,0 +1,20 @@
+//
+// Created by иван on 27.11.2022.
+//
+
+#ifndef KURSACH_TABLE_H
+#define KURSACH_TABLE_H
+
+
+class Table {
+private:
+    char* name;//название
+    char* date;//дата
+    char* num;//номер
+    int price;//цена
+public:
+
+};
+
+
+#endif //KURSACH_TABLE_H
